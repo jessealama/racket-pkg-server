f7010006af78d973d95e765b25f30e3ac1438d89
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-09-24 17:19:14 -0400

    Bring configs of pkg-index and racket-pkg-website closer to harmony

--- config-racket-pkg-website.rkt
+++ config-racket-pkg-website.rkt
@@ -4,10 +4,11 @@
 (define var "/var/lib/pkgserver")
 (main (hash 'reloadable? #t
             'var-path var
-            'package-index-url "https://localhost:9004/pkgs-all.json.gz"
+            ;; 'package-index-url "https://localhost:9004/pkgs-all.json.gz"
+            'package-index-url "file:///var/lib/pkgserver/public_html/pkg-index-static/pkgs-all.json.gz"
             'static-content-target-directory (build-path var "public_html/pkg-catalog-static")
             ;; 'static-urlprefix "https://localhost/~tonyg/pkg-catalog-static"
-            ;; 'dynamic-urlprefix "https://localhost:8444"
+            'dynamic-urlprefix "/catalog"
             'backend-baseurl "https://localhost:9004"
             'extra-static-content-directories (list (build-path var "public_html/pkg-index-static"))
             ))
