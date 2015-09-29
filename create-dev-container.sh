0fefdf41220afdeed8065ed70c985369fc9ea74b
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-09-29 12:23:33 -0400

    Include configs for docker in pkg-index and racket-pkg-website dev setup

--- create-dev-container.sh
+++ create-dev-container.sh
@@ -3,5 +3,7 @@ exec docker create \
      -p 8443:443 \
      -v `pwd`/../pkg-index:/usr/local/pkg-index \
      -v `pwd`/../racket-pkg-website:/usr/local/racket-pkg-website \
+     -v `pwd`/config-pkg-index.rkt:/usr/local/pkg-index/official/configs/docker.rkt \
+     -v `pwd`/config-racket-pkg-website.rkt:/usr/local/racket-pkg-website/configs/docker.rkt \
      -v `pwd`/data:/var/lib/pkgserver \
      pkg
