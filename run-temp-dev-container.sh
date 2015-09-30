c0b19d641e7e4ce6e44c009e6bd17786d44ded85
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-09-30 01:13:57 -0400

    Rapid apache configuration editing during development

--- run-temp-dev-container.sh
+++ run-temp-dev-container.sh
@@ -5,5 +5,6 @@ exec docker run \
      -v `pwd`/../racket-pkg-website:/usr/local/racket-pkg-website \
      -v `pwd`/config-pkg-index.rkt:/usr/local/pkg-index/official/configs/docker.rkt \
      -v `pwd`/config-racket-pkg-website.rkt:/usr/local/racket-pkg-website/configs/docker.rkt \
+     -v `pwd`/config-apache-proxy.conf:/etc/apache2/sites-available/apache-proxy.conf \
      -v `pwd`/data:/var/lib/pkgserver \
      pkg
